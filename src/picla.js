99a895248ed00af9ea336d7b95626d1a270c98a2
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-05-29 20:58:46 +0530

    Wrapper logic for images

--- src/picla.js
+++ src/picla.js
@@ -5,5 +5,18 @@
 */
 
 (function() {
-	console.log($);
+	var elements = $('.picla');
+
+	for(var i=0; i<elements.length; i++) {
+
+		var imgElement = elements[i];
+		console.log(imgElement.alt);
+
+		var parent = imgElement.parentElement;
+		console.log(parent);
+
+		var wrapper = document.createElement('div');
+
+		parent.replaceChild(wrapper, imgElement);
+	}
 })();
