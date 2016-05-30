1b9e51616857a03a378dd6ce3c3c48185e331d28
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-05-31 00:07:13 +0530

    Append actual image element to wrapper

--- src/picla.js
+++ src/picla.js
@@ -5,18 +5,28 @@
 */
 
 (function() {
-	var elements = $('.picla');
 
+	// Get all image elements with class of 'picla'
+	var elements = $('img.picla');
+
+	// Loop through all those elements and convert to label wrapper
 	for(var i=0; i<elements.length; i++) {
 
-		var imgElement = elements[i];
-		console.log(imgElement.alt);
+		var imgElement = elements[i], // Get current image element
+			imgElementAltText = imgElement.alt, // Get alt text of current image element
+			displayStyle = window.getComputedStyle(imgElement).display; // Get display style of current image element
 
 		var parent = imgElement.parentElement;
-		console.log(parent);
 
 		var wrapper = document.createElement('div');
+		wrapper.style.display = displayStyle;
+
+		var img = document.createElement('img');
+		img.src = imgElement.src;
+
+		wrapper.appendChild(img);
 
 		parent.replaceChild(wrapper, imgElement);
+
 	}
 })();
