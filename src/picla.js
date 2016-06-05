12d487e6559c8c8f238983d6e93bd68a5dd8d7da
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-06-05 18:45:44 +0530

    Image element width calculation fix

--- src/picla.js
+++ src/picla.js
@@ -6,13 +6,12 @@
 
 (function() {
 
-	"use strict";
+    "use strict";
 
     // Constructor function to create image element 
-    function ImageElement(elem, altText, width) {
+    function ImageElement(elem, altText) {
         this.elem = elem; // Current image element
         this.altText = altText; // Element's alt text
-        this.width = width; // Element's width
     }
 
     // Function to get class string of current image
@@ -51,96 +50,108 @@
     // Function to generate image labels for all image elements
     function generateImageLabel(imgElement) {
 
-    	// Get parent element of image element 
-        var parent = imgElement.getParent();
+        // Fire interval to check computed width of image
+        var interval = setInterval(function() {
 
-        // Create wrapper for image element to be rendered
-        var wrapper = document.createElement('div');
+            // If its calculated then clear inteval and run process
+            if (window.getComputedStyle(imgElement.elem).width !== '0px') {
+                clearInterval(interval);
 
-        // Add default CSS to wrapper
-        $(wrapper).css({
-            'display': 'inline-block',
-            'position': 'relative',
-            'width': imgElement.width,
-            'overflow': 'hidden',
-            'margin': getComputedStyle(imgElement.elem).margin,
-            'padding': getComputedStyle(imgElement.elem).padding
-        });
+                // Get computed width of image element
+                var width = window.getComputedStyle(imgElement.elem).width;
 
-        // Add image element classes to the wrapper
-        $(wrapper).addClass(imgElement.getClassString());
+                // Get parent element of image element 
+                var parent = imgElement.getParent();
 
-        // Create label element
-        var label = document.createElement('div');
-        $(label).css({
-        	'position': 'absolute',
-        	'bottom': 0,
-        	'width': '100%',
-        	'box-sizing': 'border-box'
-        })
-        $(label).html(imgElement.altText);
+                // Create wrapper for image element to be rendered
+                var wrapper = document.createElement('div');
 
-        // Check for label class attribute
-        var labelClass = $(imgElement.elem).attr('data-label-class');
-        if(labelClass !== '') {
-        	$(label).addClass(labelClass);
-        }
+                // Add default CSS to wrapper
+                $(wrapper).css({
+                    'display': 'inline-block',
+                    'position': 'relative',
+                    'width': width,
+                    'overflow': 'hidden',
+                    'margin': getComputedStyle(imgElement.elem).margin,
+                    'padding': getComputedStyle(imgElement.elem).padding
+                });
 
-        // Check for wrapper class attribute
-        var wrapperClass = $(imgElement.elem).attr('data-wrapper-class');
-        if(wrapperClass !== '') {
-        	$(wrapper).addClass(wrapperClass);
-        }
+                // Add image element classes to the wrapper
+                $(wrapper).addClass(imgElement.getClassString());
 
-        // Create image element to be rendered
-        var img = document.createElement('img');
+                // Create label element
+                var label = document.createElement('div');
+                $(label).css({
+                    'position': 'absolute',
+                    'bottom': 0,
+                    'width': '100%',
+                    'box-sizing': 'border-box'
+                })
+                $(label).html(imgElement.altText);
 
-        // Initialise rendered image element
-        img.src = imgElement.getSrc();
-        $(img).css({
-        	'width': '100%',
-        	'margin': 0,
-        	'padding': 0
-        });
+                // Check for label class attribute
+                var labelClass = $(imgElement.elem).attr('data-label-class');
+                if (labelClass !== '') {
+                    $(label).addClass(labelClass);
+                }
 
-        // Check for label hover attribute
-        var labelHover = $(imgElement.elem).attr('data-label-hover');
-        if(labelHover !== undefined) {
-        	var duration = '.3s'; // Set default transition duration
+                // Check for wrapper class attribute
+                var wrapperClass = $(imgElement.elem).attr('data-wrapper-class');
+                if (wrapperClass !== '') {
+                    $(wrapper).addClass(wrapperClass);
+                }
 
-        	if(labelHover !== '') {
-        		duration = labelHover; // Set custom transition duration
-        	}
+                // Create image element to be rendered
+                var img = document.createElement('img');
 
-        	$(label).css({
-        		'bottom': '-40%',
-        		'transition': 'all '+duration+' ease-in-out'
-        	});
+                // Initialise rendered image element
+                img.src = imgElement.getSrc();
+                $(img).css({
+                    'width': '100%',
+                    'margin': 0,
+                    'padding': 0
+                });
 
-        	// Hide/show label on mouseover event
-        	$(wrapper).mouseover(function() {
-        		$(label).css('bottom', '0');
-        	});
-        	$(wrapper).mouseout(function() {
-        		$(label).css('bottom', '-40%');
-        	});
-        }
+                // Check for label hover attribute
+                var labelHover = $(imgElement.elem).attr('data-label-hover');
+                if (labelHover !== undefined) {
+                    var duration = '.3s'; // Set default transition duration
 
-        // Append generated image and label to wrapper
-        wrapper.appendChild(img);
-        wrapper.appendChild(label);
+                    if (labelHover !== '') {
+                        duration = labelHover; // Set custom transition duration
+                    }
 
-        // Replace original image element with wrapper
-        parent.replaceChild(wrapper, imgElement.elem);
+                    $(label).css({
+                        'bottom': '-40%',
+                        'transition': 'all ' + duration + ' ease-in-out'
+                    });
+
+                    // Hide/show label on mouseover event
+                    $(wrapper).mouseover(function() {
+                        $(label).css('bottom', '0');
+                    });
+                    $(wrapper).mouseout(function() {
+                        $(label).css('bottom', '-40%');
+                    });
+                }
+
+                // Append generated image and label to wrapper
+                wrapper.appendChild(img);
+                wrapper.appendChild(label);
+
+                // Replace original image element with wrapper
+                parent.replaceChild(wrapper, imgElement.elem);
+            }
+        }, 1);
     };
 
     // Loop through all those elements and convert to a wrapper with attached image label
     for (var i = 0; i < elements.length; i++) {
 
         // Create new image element
-        var imgElement = new ImageElement(elements[i], elements[i].alt, window.getComputedStyle(elements[i]).width);
+        var imgElement = new ImageElement(elements[i], elements[i].alt);
 
         // Genrate image labels for all elements
-     	generateImageLabel(imgElement);
+        generateImageLabel(imgElement);
     }
 })();
